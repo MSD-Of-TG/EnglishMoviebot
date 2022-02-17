551666cf4a2dc86943bad5ef401708ddbb32c39c
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2022-02-17 00:23:22 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -74,7 +74,7 @@ This module only works for my admins
 
 <b>Commands and Usage:</b>
 • /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
+• /status - <code>to get status of files in db.</code>
 • /delete - <code>to delete a specific file from db.</code>
 • /users - <code>to get list of my users and ids.</code>
 • /chats - <code>to get list of the my chats and ids </code>
